d6aba77ac11718dc3ac79259e22d2518a54f42a5
Andres Barreto <andres.barreto@koombea.com>
2021-10-06 20:27:26 -0500

    tasks list functions

--- main.go
+++ main.go
@@ -13,8 +13,29 @@ type TaskList struct {
 	owner string
 }
 
-func (taksList *TaskList) AddTask(task *Task) {
-	taksList.tasks = append(taksList.tasks, task)
+func (tasksList *TaskList) AddTask(task *Task) {
+	tasksList.tasks = append(tasksList.tasks, task)
+}
+
+func (tasksList *TaskList) RemoveTask(index int) {
+	tasksList.tasks = append(tasksList.tasks[:index], tasksList.tasks[index+1:]...)
+}
+
+func (tasksList *TaskList) Print() {
+	for _, task := range tasksList.tasks {
+		fmt.Println("Name: ", task.name)
+		fmt.Println("Description: ", task.description)
+		fmt.Println("Completed?: ", task.complete)
+	}
+}
+
+func (tasksList *TaskList) PrintComplete() {
+	for _, task := range tasksList.tasks {
+		if task.complete {
+			fmt.Println("Name: ", task.name)
+			fmt.Println("Description: ", task.description)
+		}
+	}
 }
 
 func (t *Task) Complete() {
@@ -44,21 +65,29 @@ func NewTaskList(owner string) *TaskList {
 	}
 }
 func main() {
-	task := NewTask("Finish go course", "Finish my go course from Platzi")
+	task0 := NewTask("Finish go course", "Finish my go course from Platzi")
+	task1 := NewTask("Finish Python course", "Finish my Python course from Platzi")
+	task2 := NewTask("Finish Javascript course", "Finish my Javascript course from Platzi")
 
-	fmt.Printf("%+v\n", task)
-	task.UpdateName("Finish go course!!!")
-	fmt.Printf("%+v\n", task)
-	task.UpdateDescription("Finish my go course from Platzi!!!")
-	fmt.Printf("%+v\n", task)
-	task.Complete()
-	fmt.Printf("%+v\n", task)
+	fmt.Printf("%+v\n", task0)
+	task0.UpdateName("Finish go course!!!")
+	task0.UpdateDescription("Finish my go course from Platzi!!!")
+	task0.Complete()
+	fmt.Printf("%+v\n", task0)
 
 	taskList := NewTaskList("Andres")
 	fmt.Printf("%+v\n", taskList)
-	taskList.AddTask(task)
+	taskList.AddTask(task0)
+	taskList.AddTask(task1)
+	taskList.AddTask(task2)
 	fmt.Printf("%+v\n", taskList)
 	fmt.Println(taskList.tasks)
 	fmt.Println(taskList.tasks[0])
+	taskList.RemoveTask(1)
+	fmt.Println(taskList.tasks)
+	taskList.AddTask(task1)
+	fmt.Println(taskList.tasks)
+	taskList.Print()
+	taskList.PrintComplete()
 
 }
